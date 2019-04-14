8ec69ac921447af41f877020a915474969df3743
Marek Rewers <mrewers@users.noreply.github.com>
2019-04-14 14:04:11 -0400

    Add common js modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import nav from "./nav";
 import { top, bottom } from './footer';
+import makeButton from './button';
+import { makeColorStyle } from './button-styles';
 
-console.log(nav(), top, bottom);
+console.log(
+  nav(),
+  top,
+  bottom,
+  makeButton(button),
+  makeColorStyle('orange')
+);
