174dc0e6cbe7894f7d5f0819b805e6c2448e925f
Marek Rewers <mrewers@users.noreply.github.com>
2019-04-14 17:40:14 -0400

    Add dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import { footer } from './footer';
 import makeButton from './button';
 import { makeColorStyle } from './button-styles';
 import makeImage from './image.js';
 
-import './button.css';
+import imageUrl from './icon.jpeg'
 
-import imageUrl from "./icon.jpeg"
+const loadFooter = () => import('./footer')
+import './button.css';
 
 const image = makeImage(imageUrl)
 
 const button = makeButton('hello');
+
+button.addEventListener('click', e => {
+  loadFooter().then(m => {
+    document.body.appendChild(m.footer);
+  })
+})
 button.style = makeColorStyle('orange');
 document.body.appendChild(button);
 document.body.appendChild(image);
-document.body.appendChild(footer);
