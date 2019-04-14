f6a0d58d95cc7c69725251487e4554034f816a0a
Marek Rewers <mrewers@users.noreply.github.com>
2019-04-14 16:54:50 -0400

    Add url loader

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import { footer } from './footer';
 import makeButton from './button';
 import { makeColorStyle } from './button-styles';
+import makeImage from './image.js';
 
 import './button.css';
+
+import imageUrl from "./icon.jpeg"
+
+const image = makeImage(imageUrl)
 
 const button = makeButton('hello');
 button.style = makeColorStyle('orange');
 document.body.appendChild(button);
+document.body.appendChild(image);
 document.body.appendChild(footer);
